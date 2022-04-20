cf1562ceb7fffa5dc650c8daa03fa4892e6e40d3
Leonardo Carrasco <lcarrasco@nearsoft.com>
2022-04-20 10:05:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Instructions } from './game/Instructions'
+import { Play } from './game/Play'
+
+const initialState = {
+  isPlaying: false,
+}
+
 function App() {
+  const [state, setState] = React.useState(initialState);
+  const onPlay = (isPlaying) => setState(state => ({...state, isPlaying}))
+  const { isPlaying } = state;
   return (
     <div className="App">
       <header className="App-header">
+        {isPlaying ? <Play goBack={() => onPlay(false)} /> : <Instructions onPlay={() => onPlay(true)} />}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
